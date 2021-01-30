b2962e5f4868765d140c2aecebb66a914767dceb
Yuri Araújo <yuri.hassle.araujo@gmail.com>
2021-01-30 03:56:10 -0400

    chore: add vuetify

--- src/services.js
+++ src/services.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import Swal from 'sweetalert2';
 import router from './router';
-import store from './store';
+import store from './plugins/store';
 
 const axiosInstance = axios.create({
   //baseURL: 'https://covid-vaccine-server.herokuapp.com/api',
