87111039de779332f5ce57afc71033cc9ecd528d
Yuri Araújo <yuri.hassle.araujo@gmail.com>
2021-02-06 19:15:35 -0400

    refactor: remove edit route

--- src/plugins/router.js
+++ src/plugins/router.js
@@ -1,7 +1,6 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 import Home from '../views/Home';
-import EditApplication from '../views/EditApplication';
 import Login from '../views/Login';
 import Swal from 'sweetalert2';
 
@@ -15,11 +14,6 @@ const router = new Router({
       path: '/',
       name: 'home',
       component: Home,
-    },
-    {
-      path: '/editar',
-      name: 'edit',
-      component: EditApplication,
     },
     {
       path: '/login',
