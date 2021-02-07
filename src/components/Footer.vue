8fd2ec84dbba4beb362664b18b9aed2494c59e5a
Yuri Araújo <yuri.hassle.araujo@gmail.com>
2021-02-07 19:01:12 -0400

    styling: change footer and header styling

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,30 +1,22 @@
 <template>
-  <footer>
-    <div class="footer-container">
-      <div class="footer-section">
-        <h3>Endereço</h3>
+  <footer class="pa-8">
+    <div class="container">
+      <div class="footer-text">
         <p>
-          Avenida Mário Ypiranga Monteiro, 1695 - Adrianópolis
+          © 2021 - Secretaria Municipal de Saúde - SEMSA
         </p>
         <p>
-          Cep: 69057-001
+          Desenvolvido pelo Departamento de Tecnologia da Informação - DTI.
         </p>
       </div>
-      <div class="footer-section">
-        <h3>Link</h3>
-        <p>
-          <a href="https://semsa.manaus.am.gov.br/"
-            >Secretaria Municipal de Saúde de Manaus</a
-          >
-        </p>
-        <p><a href="http://www.manaus.am.gov.br/">Prefeitura de Manaus</a></p>
+      <div class="footer-images">
+        <a href="https://covid19.manaus.am.gov.br/">
+          <img src="../assets/covid_combat.png" />
+        </a>
+        <a href="https://semsa.manaus.am.gov.br/">
+          <img src="../assets/semsa_logo.png" />
+        </a>
       </div>
-    </div>
-    <div class="copyright">
-      <p>
-        © 2021 - Secretaria Municipal de Saúde - SEMSA
-      </p>
-      <p>Desenvolvido pelo Departamento de Tecnologia da Informação - DTI</p>
     </div>
   </footer>
 </template>
@@ -34,39 +26,34 @@
 </script>
 
 <style scoped lang="scss">
-  p {
-    margin-bottom: 5px;
-  }
-  .copyright {
-    p {
-      color: rgba(255, 255, 255, 0.8);
-    }
-    background-color: #005346;
-    margin-left: 20px;
-  }
   footer {
-    background-color: #005346;
-    color: #f5f5f5;
-    padding: 15px 10px 10px 5px;
+    background-color: #0ca3ab;
+    background-image: url(../assets/bg-footer.png);
+    background-position: bottom left;
+    background-repeat: repeat-x;
   }
-  .footer-container {
-    display: flex;
-    align-items: flex-start;
-    justify-content: space-evenly;
+
+  p {
+    margin-top: 5px;
+    margin-bottom: 5px;
+    color: #fff;
   }
-  .footer-section {
-    padding: 20px 20px;
-  }
-  @media screen and (max-width: 500px) {
-    .footer-container {
-      flex-direction: column;
-    }
-    .footer-section {
-      padding: 10px 10px;
+
+  .footer-images {
+    a {
+      margin-left: 40px;
     }
   }
 
-  h3 {
-    margin-bottom: 10px;
+  .container {
+    display: flex;
+    justify-content: space-between;
+    margin-bottom: 40px;
+  }
+
+  @media screen and (max-width: 500px) {
+    footer {
+      flex-direction: column;
+    }
   }
 </style>
